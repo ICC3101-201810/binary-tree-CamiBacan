d138464def171a77cd971fa632c93184bd72f23f
CamiBacan <cesepulveda@miuandes.cl>
2018-04-24 17:13:58 -0300

    First commit

---
+++ Camila Sepulveda/Camila Sepulveda/Node.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camila_Sepulveda
+{
+    class Node<T>
+    {
+        T data = default(T);
+        Node<T> left = default(Node<T>);
+        Node<T> right = default(Node<T>);
+
+        /*public Node(T data, T leftNode, Node<T> rightNode)
+        {
+            this.data = data;
+            left = leftNode;
+            right = rightNode;
+        }*/
+        public Node(T node)
+        {
+            data = node;
+            left = null;
+            right = null;
+        }
+
+        public Node()
+        {
+            data = default(T);
+            left = null;
+            right = null;
+        }
+
+        public T GetData()
+        {
+            return data;
+        }
+        
+
+        public Node<T> GetLeft()
+        {
+            return left;
+        }
+
+        public Node<T> GetRight()
+        {
+            return right;
+        }
+
+        public LinkedListNode<T> AddData(T obj)
+        {
+            LinkedListNode<T> dataList = new LinkedListNode<T>(obj);
+            return dataList;
+        }
+
+         
+    }          
+    
+}
---
+++ Camila Sepulveda/Camila Sepulveda/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camila_Sepulveda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> n1 = new Node<int>();
+            Node<int> n2 = new Node<int>();
+            n1.AddData(15);
+            Console.WriteLine(n1.GetT());
+
+            Console.ReadLine();
+
+        }
+    }
+}
